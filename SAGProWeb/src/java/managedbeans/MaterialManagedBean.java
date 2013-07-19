6b5b6aa9033b7bc7b30a2b290c7e98f1f78c7714
Jose Luis <jose.varela@usach.cl>
2013-07-19 14:23:41 -0400

    Managed Bean de Objetivos

--- SAGProWeb/src/java/managedbeans/MaterialManagedBean.java
+++ SAGProWeb/src/java/managedbeans/MaterialManagedBean.java
@@ -14,6 +14,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ValueChangeEvent;
 import sessionbeans.MaterialFacadeLocal;
+import org.primefaces.model.MenuModel;
 
 /**
  *
