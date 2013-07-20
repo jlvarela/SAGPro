243bf8d4cab127a2d5591610cc9588bbc8f49829
Jose Luis <jose.varela@usach.cl>
2013-07-20 16:05:31 -0400

    Resolvierndo Merge

--- SAGProWeb/src/java/managedbeans/UserManagedBean.java
+++ SAGProWeb/src/java/managedbeans/UserManagedBean.java
@@ -7,7 +7,6 @@ package managedbeans;
 import entities.User;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
@@ -22,9 +21,9 @@ import sessionbeans.UserFacadeLocal;
 @Named(value = "userManagedBean")
 @SessionScoped
 public class UserManagedBean implements Serializable {
+
     @EJB
     private UserFacadeLocal userFacade;
-    
 
     public String getUserid() {
         return userid;
@@ -50,56 +49,33 @@ public class UserManagedBean implements Serializable {
         this.userlastname = userlastname;
     }
 
-    public List<User> getListaUsers() {
-        return listaUsers;
-    }
-
-    public void setListaUsers(List<User> listaUsers) {
-        this.listaUsers = listaUsers;
-    }
-
-    public List<User> getSelectedUsers() {
-        return selectedUsers;
-    }
-
-    public void setSelectedUsers(List<User> selectedUsers) {
-        this.selectedUsers = selectedUsers;
-    }
-    
-    
-    
     private String userid;
     private String username;
     private String userlastname;
     private String usermail;
-    private List<User> listaUsers;
-    private List<User> selectedUsers;
 
-   
-    
+
     /**
      * Creates a new instance of UserManagedBean
      */
     public UserManagedBean() {
     }
-    
-    
-    
+
     @PostConstruct
-    public void init(){
+    public void init() {
         FacesContext context = FacesContext.getCurrentInstance();
         HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
-        this.userid = request.getUserPrincipal().getName();
-        User usuario = null;
-        if (this.userid != null)
-            usuario = userFacade.buscarPorRut(userid);
-        if (usuario != null){
-            this.username = usuario.getNombreUser();
-            this.userlastname = usuario.getApellidoUser();
-            this.usermail = usuario.getEmailUser();
-        } 
-        
-        listaUsers=userFacade.findAll();        
-        System.out.println(listaUsers.size());
+        if (request.getUserPrincipal() != null) {
+            this.userid = request.getUserPrincipal().getName();
+            User usuario = null;
+            if (this.userid != null) {
+                usuario = userFacade.buscarPorRut(userid);
+            }
+            if (usuario != null) {
+                this.username = usuario.getNombreUser();
+                this.userlastname = usuario.getApellidoUser();
+                this.usermail = usuario.getEmailUser();
+            }
+        }
     }
 }
