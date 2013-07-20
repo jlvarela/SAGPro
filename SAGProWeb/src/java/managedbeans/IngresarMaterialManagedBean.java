e781084024d8fdb6d45aadc00241de02e9c983de
Gary Fuenzalida <gary.fuenzalida@usach.cl>
2013-07-20 13:54:55 -0400

    ManagedBeans Materiales, Login bonito funcional
    Creados Managed Beans para Ingresar y Consultar material.
    La apariencia del login antiguo, fue reemplazada por el login de Marco.
    Aplicados cambios en Managed Beans para Ingresar y Consultar material
    sugeridos por Jose.

---
+++ SAGProWeb/src/java/managedbeans/IngresarMaterialManagedBean.java
@@ -0,0 +1,126 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import entities.Material;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import sessionbeans.MaterialFacadeLocal;
+
+/**
+ *
+ * @author Gary
+ */
+@ManagedBean (name = "ingresarMaterialManagedBean")
+@ViewScoped
+public class IngresarMaterialManagedBean {
+
+    /**
+     * Creates a new instance of IngresarMaterialManagedBean
+     */
+    public IngresarMaterialManagedBean() {
+    }
+    
+    @EJB
+    private MaterialFacadeLocal materialFacade;
+
+    /**
+     * Creates a new instance of MaterialManagedBean
+     */
+
+    @PostConstruct
+    public void init() {
+        String[] valuesVentas = materialFacade.getValuesVentaMaterial();
+        String[] valuesProduc = materialFacade.getValuesProducMaterial();
+        if (valuesVentas != null && valuesProduc != null){
+            medidasVentaMaterial = valuesVentas;
+            medidasProduccionMaterial = valuesProduc;
+        }
+        
+        listaMateriales = materialFacade.findAll();
+    }
+
+    public List<Material> getListaMateriales() {
+        return listaMateriales;
+    }
+
+    public void setListaMateriales(List<Material> listaMateriales) {
+        this.listaMateriales = listaMateriales;
+    }
+    private String nombreMaterial;
+    private String medidaProduccionMaterial;
+    private String medidaVentaMaterial;
+    private String[] medidasVentaMaterial;
+    private String[] medidasProduccionMaterial;
+    private List<Material> listaMateriales;
+
+    public String[] getMedidasVentaMaterial() {
+        return medidasVentaMaterial;
+    }
+
+    public void setMedidasVentaMaterial(String[] medidasVentaMaterial) {
+        this.medidasVentaMaterial = medidasVentaMaterial;
+    }
+
+    public String[] getMedidasProduccionMaterial() {
+        return medidasProduccionMaterial;
+    }
+
+    public void setMedidasProduccionMaterial(String[] medidasProduccionMaterial) {
+        this.medidasProduccionMaterial = medidasProduccionMaterial;
+    }
+
+    public String getNombreMaterial() {
+        return nombreMaterial;
+    }
+
+    public void setNombreMaterial(String nombreMaterial) {
+        this.nombreMaterial = nombreMaterial;
+    }
+
+    public String getMedidaProduccionMaterial() {
+        return medidaProduccionMaterial;
+    }
+
+    public void setMedidaProduccionMaterial(String medidaProduccionMaterial) {
+        this.medidaProduccionMaterial = medidaProduccionMaterial;
+    }
+
+    public String getMedidaVentaMaterial() {
+        return medidaVentaMaterial;
+    }
+
+    public void setMedidaVentaMaterial(String medidaVentaMaterial) {
+        this.medidaVentaMaterial = medidaVentaMaterial;
+    }
+
+    public void ingresarMaterial() {
+        System.out.println(nombreMaterial+medidaProduccionMaterial+medidaVentaMaterial);
+        int resp = materialFacade.agregarMaterial(nombreMaterial, medidaProduccionMaterial, medidaVentaMaterial);
+        FacesContext fcontext = FacesContext.getCurrentInstance();
+        if (resp == 0) {
+            fcontext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", "Material agregado con éxito"));
+        } else if (resp == -1) {
+            fcontext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "Material ya ingresado"));
+        }
+    }
+    
+    public void materialSelectCantidadListener(ValueChangeEvent event){
+        String idMatSelect = (String) event.getNewValue();
+        for(Material material: listaMateriales){
+            if(material.getCodMaterial().equals(Integer.parseInt(idMatSelect))){
+                medidaProduccionMaterial = material.getMedidaProduccionMaterial();
+                System.out.println("new value " + medidaProduccionMaterial);
+                break;
+            }
+        }
+    }
+}
