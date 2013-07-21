d18b2d2d544f5e2b09737292ac88ee5c62719500
Jose Luis <jose.varela@usach.cl>
2013-07-21 00:12:52 -0400

    Entity Class actualizadas según DB

--- SAGProApp/SAGProApp-ejb/src/java/sessionbeans/ObjetivoFacade.java
+++ SAGProApp/SAGProApp-ejb/src/java/sessionbeans/ObjetivoFacade.java
@@ -5,7 +5,10 @@
 package sessionbeans;
 
 import entities.Objetivo;
+import java.util.Date;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -25,6 +28,54 @@ public class ObjetivoFacade extends AbstractFacade<Objetivo> implements Objetivo
 
     public ObjetivoFacade() {
         super(Objetivo.class);
+    }
+
+    @Override
+    public void create(Objetivo entity) {
+        super.create(entity); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Objetivo> findAll() {
+        return super.findAll(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int agregarObjetivo(final String nombre
+            , final String decripcion
+            , final Date fecha_inicial
+            , final Date fecha_final
+            , final short prioridad) {
+        
+        Objetivo obj = new Objetivo();
+        obj.setNombreObjetivo(nombre);
+        obj.setDescripcionObjetivo(decripcion);
+        obj.setFechaInicial(fecha_inicial);
+        obj.setFechaLimite(fecha_final);
+        obj.setPrioridadObjetivo(prioridad);
+        
+        if (!objetivoExists(nombre)){
+            try{
+                create(obj);
+                System.out.println("Creación de Objetivo realizada con éxito");
+                return 0;
+            }catch(EntityExistsException e){
+                System.out.println("Ingresando objetivo:" + e.getMessage());
+                return -1;
+            }
+        }
+        else{
+            return -1;
+        }
+    }
+    
+    public Boolean objetivoExists(String obj_nombre){
+        int resultados;
+        resultados = em.createNamedQuery("Objetivo.findByNombreObjetivo")
+                .setParameter("nombreObjetivo", obj_nombre)
+                .getResultList().size();
+        
+        return resultados != 0;
     }
     
 }
