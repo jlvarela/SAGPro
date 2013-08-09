b0f4d6f2a7170c26054cacd321105cdba88d4570
Jose Luis <jose.varela@usach.cl>
2013-08-08 20:34:09 -0400

    Evolución de Producción. Ahora se muestra la producción para cualquier mes y material escogido. Aún no se puede ver por año o los objetivos.

--- SAGProWeb/src/java/managedbeans/EvolucionManagedBean.java
+++ SAGProWeb/src/java/managedbeans/EvolucionManagedBean.java
@@ -6,15 +6,18 @@ package managedbeans;
 
 import java.io.Serializable;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ValueChangeEvent;
 import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.LineChartSeries;
 import pojoclass.Material;
@@ -37,21 +40,48 @@ public class EvolucionManagedBean implements Serializable{
     private ObjetivoFacadeLocal objetivoFacade;
     @EJB
     private MaterialFacadeLocal materialFacade;
-
+    
+    private final DateFormat dayFormat = new SimpleDateFormat("dd");                // DateFormat para días
+    private DateFormat monthFormat = new SimpleDateFormat("MM/yyyy");               // DateFormat para mes
+    private List<Material> materialList;                                            // Listado de Materiales
+    private List<Objetivo> objectiveList;                                           // Listado de Objetivos
+    private Material selectedMaterial;                                              // Material seleccionado
+    private CartesianChartModel model;                                              // Modelo del gráfico
+    private Calendar selectedDate;                                                      // Fecha seleccionada
+    private String formattedDate;                                                   // Fecha seleccionada con formato
+    
     /**
      * Creates a new instance of EvolucionManagedBean
      */
-    
-    private final static DateFormat dateFormat = new SimpleDateFormat("dd");
-    private List<Material> listaMateriales;
-    private List<Objetivo> listaObjetivos;
-    private Material selectedMaterial;
-    private CartesianChartModel model;
-    
     public EvolucionManagedBean() {
         
     }
 
+    public Calendar getSelectedDate() {
+        return selectedDate;
+    }
+
+    public void setSelectedDate(Calendar selectedDate) {
+        this.selectedDate = selectedDate;
+    }
+
+    public String getFormattedDate() {
+        return formattedDate;
+    }
+
+    public void setFormattedDate(String formattedDate) {
+        this.formattedDate = formattedDate;
+    }
+
+    
+    public DateFormat getMonthFormat() {
+        return monthFormat;
+    }
+
+    public void setMonthFormat(DateFormat monthFormat) {
+        this.monthFormat = monthFormat;
+    }
+        
     public CartesianChartModel getModel() {
         return model;
     }
@@ -70,93 +100,126 @@ public class EvolucionManagedBean implements Serializable{
     }
 
     public List<Objetivo> getListaObjetivos() {
-        return listaObjetivos;
+        return objectiveList;
     }
 
     public void setListaObjetivos(List<Objetivo> listaObjetivos) {
-        this.listaObjetivos = listaObjetivos;
+        this.objectiveList = listaObjetivos;
     }
 
     public List<Material> getListaMateriales() {
-        return listaMateriales;
+        return materialList;
     }
 
     public void setListaMateriales(List<Material> listaMateriales) {
-        this.listaMateriales = listaMateriales;
+        this.materialList = listaMateriales;
     }
     
     @PostConstruct
     public void init(){
         rellenarListas();
-        createLinearChart();
-        
+        selectedDate = Calendar.getInstance();
+        createLinearChart(selectedDate);
+        formattedDate = monthFormat.format(selectedDate.getTime());
     }
     
+    /**
+     * 
+     */
     private void rellenarListas(){
+        // Lista de Materiales para selección
         List<entities.Material> materialEntitiesList = materialFacade.findAll();
-        ArrayList<Material> materialList = new ArrayList();
-        for (entities.Material matEntitie : materialEntitiesList){
-            materialList.add(util.MappingFromEntitieToPojo.materialFromEntityToPojo(matEntitie));
-        }
-        listaMateriales = materialList;
-        if (! listaMateriales.isEmpty() )
-            selectedMaterial = listaMateriales.get(0);
+        ArrayList<Material> materialArrayList = new ArrayList();
         
+        // Mapping de clases Entity a POJO
+        for (entities.Material matEntitie : materialEntitiesList){
+            materialArrayList.add(util.MappingFromEntitieToPojo.materialFromEntityToPojo(matEntitie));
+        }
+        
+        // Actualizar lista de materiales
+        this.materialList = materialArrayList;
+        
+        // Auto-selector de material
+        if (! this.materialList.isEmpty() ){
+            selectedMaterial = this.materialList.get(0);
+        }
+        
+        // Lista de Objetivo para seleccionar
         List<entities.Objetivo> objetivoEntitiesList = objetivoFacade.findAll();
         ArrayList<Objetivo> objetivoList = new ArrayList();
+        
+        // Mapping de clases Entity a POJO
         for (entities.Objetivo objEntity : objetivoEntitiesList){
             objetivoList.add(util.MappingFromEntitieToPojo.objetivoFromEntityToPojo(objEntity));
         }
-        listaObjetivos = objetivoList;        
+        
+        // Actualizar lista de objetivos
+        objectiveList = objetivoList;        
     }
     
-    private void createLinearChart(){
-        // Por ahora sólo calcula por mes actual
-        Calendar cal = Calendar.getInstance();
-        int daysInMonth;
-        daysInMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+    /**
+     * 
+     */
+    private void createLinearChart(Calendar date){
+        model = new CartesianChartModel();
         
-        // Último día del mes.
-        cal.set(Calendar.DAY_OF_MONTH, daysInMonth);
-        cal.set(Calendar.HOUR_OF_DAY, 23);
-        cal.set(Calendar.MINUTE, 59);
-        cal.set(Calendar.SECOND, 0);
+        Calendar month = (Calendar)date.clone();
+        
+        // Último día
+        int lastDay;
+        
+        // Determinar el último día del gráfico
+        // En caso de que el mes sea el actual, el último día del gráfico es hoy
+        int resp;
+        resp = compareSelectedMonthTodaysMonth(month);
+
+        if (  resp == 0 ){
+            Calendar cal = Calendar.getInstance();
+            lastDay = cal.get(Calendar.DAY_OF_MONTH);
+        }
+        else
+            lastDay = month.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        
+        // Final del calendario
+        month.set(Calendar.DAY_OF_MONTH, lastDay);
+        month.set(Calendar.HOUR_OF_DAY, 23);
+        month.set(Calendar.MINUTE, 59);
+        month.set(Calendar.SECOND, 0);
         
         Date f_final;
-        f_final = cal.getTime();
+        f_final = month.getTime();
         
         // Primer día del mes.
-        cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.set(Calendar.DAY_OF_MONTH, 1);
-        cal.clear(Calendar.MINUTE);
-        cal.clear(Calendar.SECOND);
-        cal.clear(Calendar.MILLISECOND);
+        month.set(Calendar.DAY_OF_MONTH, 1);
+        month.set(Calendar.HOUR_OF_DAY, 0);
+        month.clear(Calendar.MINUTE);
+        month.clear(Calendar.SECOND);
+        month.clear(Calendar.MILLISECOND);
         
         // Creando Date a partir de Calendar
         Date f_inicial;
-        f_inicial = cal.getTime();
-        
-        
+        f_inicial = month.getTime();
         
         List<ProduccionDiaria> prodDiariaList;
         prodDiariaList = getListaProduccionDiaria(selectedMaterial.getCodMaterial(), f_inicial, f_final);
-        
-        model = new CartesianChartModel();
-        
+               
         LineChartSeries line = new LineChartSeries();
         line.setLabel("Evolucion ".concat(selectedMaterial.getNombreMaterial()));
 
         // Creando puntos de la función
         int index;
-        for ( int i = 0; i < daysInMonth ; i++ ){
-            index = isDateInProduccionList(cal.getTime(), prodDiariaList);
-            System.out.println(index);
+        int acumulado;
+        acumulado = 0;
+        for ( int i = 0; i < lastDay ; i++ ){
+            // Bucar si se produjo algo en día i.
+            index = isDateInProduccionList(month.getTime(), prodDiariaList);
+            // Si dicho día se produjo, incrementar acumulado.
             if ( index != -1 ){
-                line.set(dateFormat.format(cal.getTime()), prodDiariaList.get(index).getCantidad());
+                acumulado += prodDiariaList.get(index).getCantidad();   // Incrementar acumulado.
             }
-            else
-                line.set(dateFormat.format(cal.getTime()), 0);
-            cal.add(Calendar.DAY_OF_MONTH, 1);
+            line.set(String.valueOf(month.get(Calendar.DAY_OF_MONTH)), acumulado);      // Agregar punto.
+            month.add(Calendar.DAY_OF_MONTH, 1);                          // Incrementar día del Calendario.
         }
         model.addSeries(line);
     }
@@ -184,6 +247,67 @@ public class EvolucionManagedBean implements Serializable{
             if ( lista.get(i).getFecha().compareTo(fecha) == 0 )
                 return i;
         }
+        return -1;
+    }
+        
+    /**
+     * Listener del evento onChange de la lista de materiales.
+     * Una vez se selecciona un nuevo material, se actualiza el
+     * atributo selectedMaterial, y se actualiza el gráfico en
+     * función de esto.
+     * @param event 
+     */
+    public void handleMaterialSelectChange(ValueChangeEvent event){
+        String idMatSelect = (String) event.getNewValue();
+        for(Material material: materialList){
+            if(material.getCodMaterial().equals(Integer.parseInt(idMatSelect))){
+                this.selectedMaterial = material;
+                break;
+            }
+        }
+        createLinearChart(selectedDate);
+    }
+    
+    /**
+     * 
+     * @param event 
+     */
+    public void handleDateSelectChange(ValueChangeEvent event){
+        String newDateString;
+        Date newDate;
+        
+        newDateString = (String) event.getNewValue();
+        try {
+            newDate = monthFormat.parse(newDateString);
+            selectedDate.setTime(newDate);
+        }
+        catch (ParseException pe){
+            System.out.println(pe.getMessage());
+        }
+        
+        createLinearChart(selectedDate);
+    }
+    
+    public String getNameOfMonth(){
+        return selectedDate.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH);
+    }
+
+    /**
+     * 
+     * @param month
+     * @return 
+     */
+    private int compareSelectedMonthTodaysMonth(Calendar month) {
+        Calendar today = Calendar.getInstance();
+        if ( (month.get(Calendar.MONTH) == today.get(Calendar.MONTH))
+                && (month.get(Calendar.YEAR) == today.get(Calendar.YEAR) ) ){
+            return 0;
+        }
+        else if ( (month.get(Calendar.YEAR) > today.get(Calendar.YEAR) ) )
+            return 1;
+        else if ( (month.get(Calendar.MONTH) > today.get(Calendar.MONTH))
+                && (month.get(Calendar.YEAR) == today.get(Calendar.YEAR) ) )
+            return 1;
         return -1;
     }
 }
