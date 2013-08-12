0460f6f520f5aa96e25aa62768a80ffaf09912c7
Jose Luis <jose.varela@usach.cl>
2013-08-12 00:01:13 -0400

    Agregando comentarios
    Comentarios...Comentarios Everywhere!

--- SAGProApp/SAGProApp-ejb/src/java/sessionbeans/MaterialFacade.java
+++ SAGProApp/SAGProApp-ejb/src/java/sessionbeans/MaterialFacade.java
@@ -1,12 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package sessionbeans;
 
 import entities.Material;
 import java.util.List;
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
@@ -20,8 +15,10 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 public class MaterialFacade extends AbstractFacade<Material> implements MaterialFacadeLocal {
-    private final String[] medidasVentasValues = {"Ton", "m3"};
-    private final String[] medidasProducValues = {"Ton", "m3"};
+    
+    private final String[] medidasVentasValues = {"Ton", "m3"}; //  Arreglo de medidas de venta.
+    private final String[] medidasProducValues = {"Ton", "m3"}; //  Arreglo de medidas de producción.
+    
     @PersistenceContext(unitName = "SAGProApp-ejbPU")
     private EntityManager em;
 
@@ -35,33 +32,34 @@ public class MaterialFacade extends AbstractFacade<Material> implements Material
     }
 
     /**
-     *
-     * @return Lista de todos los materiales
+     * Obtiene la lista de todos los materiales.
+     * @return List<Material>   Lista de todos los materiales
      */
     @Override
     public List<Material> findAll() {
-        return super.findAll(); //To change body of generated methods, choose Tools | Templates.
+        return super.findAll();
     }
 
     /**
-     *
-     * @param nombre_material
-     * @param medida_produccion_material
-     * @param medida_venta_material
-     * @return
+     * Agregar un nuevo material al sistema.
+     * @param nombre_material   Nombre del material
+     * @param medida_produccion_material    Medida de producción del material.
+     * @param medida_venta_material         Medida de venta del material.
+     * @return  int Código de verificación de la transacción.
      */
     @Override
     public int agregarMaterial(final String nombre_material, final String medida_produccion_material, final String medida_venta_material) {
+        //  Si material no se ha ingresado previamente.
         if (!materialExists(nombre_material)) {
             try {
-                Material material = new Material();
-                material.setNombreMaterial(nombre_material);
-                material.setMedidaProduccionMaterial(medida_produccion_material);
-                material.setMedidaVentaMaterial(medida_venta_material);
-                create(material);
+                Material material = new Material();                                 // Nuevo material a ingresar.
+                material.setNombreMaterial(nombre_material);                        // Setear nombre del material.
+                material.setMedidaProduccionMaterial(medida_produccion_material);   // Setear medida de producción.
+                material.setMedidaVentaMaterial(medida_venta_material);             // Setear medida de venta.
+                create(material);                                                   // Crear material
                 System.out.println("Creación de material realizada con éxito");
-                return 0;
-            } catch (EntityExistsException e) {
+                return 0;                                                           // Retornar código satisfactorio.
+            } catch (EntityExistsException e) {                                     // Si entidad ya existe.
                 System.out.println("Ingresando Material: Error -> " + e.getMessage());
                 return -1;
             }
@@ -70,9 +68,13 @@ public class MaterialFacade extends AbstractFacade<Material> implements Material
         }
     }
 
+    /**
+     * Persistir nueva entidad Material.
+     * @param entity Entidad a persistir,
+     */
     @Override
     public void create(Material entity) {
-        super.create(entity); //To change body of generated methods, choose Tools | Templates.
+        super.create(entity);
     }
 
     /**
@@ -91,10 +93,15 @@ public class MaterialFacade extends AbstractFacade<Material> implements Material
         return resultados != 0;
     }
     
-    private Boolean materialExistsByCode(String material_name) {
+    /**
+     * Determina si un material ya existe por su código.
+     * @param material_code Códigod el material a ingresar.
+     * @return  Boolean Determina si existe o no.
+     */
+    private Boolean materialExistsByCode(String material_code) {
         int resultados;
         resultados = em.createNamedQuery("Material.findByCodMaterial")
-                .setParameter("codMaterial", Long.parseLong(material_name))
+                .setParameter("codMaterial", Long.parseLong(material_code))
                 .getResultList().size();
 
         return resultados != 0;
@@ -119,7 +126,12 @@ public class MaterialFacade extends AbstractFacade<Material> implements Material
     public String[] getValuesProducMaterial() {
         return getMedidasProducValues();
     }
-
+    
+    /**
+     * Busca a un material por su ID.
+     * @param material_id
+     * @return 
+     */
     @Override
     public Material buscarPorID(final String material_id) {
         try {
@@ -147,12 +159,15 @@ public class MaterialFacade extends AbstractFacade<Material> implements Material
     public void remove(Material entity) {
         super.remove(entity); //To change body of generated methods, choose Tools | Templates.
     }
-
     
-    
+    /**
+     * Eliminar un material por su código.
+     * @param material_id   Id del material a eliminar
+     * @return int  código de satisfacción de la transacción.
+     */
     @Override
     public int eliminarMaterial(final String material_id) {
-
+        // Buscar si material existe.
         if (!materialExists(material_id)) {
             return -1;
 
@@ -169,11 +184,20 @@ public class MaterialFacade extends AbstractFacade<Material> implements Material
         }
     }
     
+    /**
+     * Editar información de un material.
+     * Para esto se ingresa el código, nuevo nombre, medida de producción y venta.
+     * 
+     * @param codMaterial
+     * @param nombre_material
+     * @param medida_produccion_material
+     * @param medida_venta_material
+     * @return 
+     */
     @Override
     public int editarMaterial(final String codMaterial, final String nombre_material, final String medida_produccion_material, final String medida_venta_material){
         if (!materialExistsByCode(codMaterial)) {
             return -1;
-            
         } else {
             try {
                 Material material=buscarPorID(codMaterial);
--- SAGProWeb/src/java/managedbeans/ConsultarObjetivoManagedBean.java
+++ SAGProWeb/src/java/managedbeans/ConsultarObjetivoManagedBean.java
@@ -28,19 +28,18 @@ import sessionbeans.ObjetivoMaterialFacadeLocal;
 @ViewScoped
 public class ConsultarObjetivoManagedBean implements Serializable {
     @EJB
-    private ObjetivoMaterialFacadeLocal objetivoMaterialFacade;
+    private ObjetivoMaterialFacadeLocal objetivoMaterialFacade;         //  Para obtener los materiales de un objetivo.
     @EJB
-    private ObjetivoFacadeLocal objetivoFacade;
-    
+    private ObjetivoFacadeLocal objetivoFacade;                         //  Para obtener la lista de objetivos.
     @EJB
-    private MaterialFacadeLocal materialFacade;
+    private MaterialFacadeLocal materialFacade;                         //  Para obtener la lista de materiales.
     
-    private List<Material> materialList;
-    private List<Objetivo> listaObjetivos;
-    private Objetivo selectedObjetivo;
-    private SimpleDateFormat sdf;
-    private Material tempSelectedMaterial;
-    private Integer cantSelectedMaterial;
+    private List<Material> materialList;                                //  Lista de materiales.
+    private List<Objetivo> listaObjetivos;                              //  Lista de objetivos.
+    private Objetivo selectedObjetivo;                                  //  Objetivo seleccionado.
+    private SimpleDateFormat sdf;                                       //  Formateador de fechas.
+    private Material tempSelectedMaterial;                              //  Material seleccionado.
+    private Integer cantSelectedMaterial;                               // Cantidad del material a añadir.
     
     /**
      * Creates a new instance of ConsultarObjetivoManagedBean
@@ -51,7 +50,7 @@ public class ConsultarObjetivoManagedBean implements Serializable {
     }
     
     /**
-     * Obtener la lista de todos los objetivos del sistema.
+     * Obtener la lista de todos los objetivos y materiales del sistema.
      * 
      */
     @PostConstruct
@@ -60,26 +59,31 @@ public class ConsultarObjetivoManagedBean implements Serializable {
         tempSelectedMaterial = new Material();
     }
     
+    /**
+     * Rellenar las listas de materiales y objetivos
+     */
     private void rellenarListas(){
-        // Lista de Materiales para selección
+        // Lista de Materiales para selección.
         List<entities.Material> materialEntitiesList = materialFacade.findAll();
         ArrayList<Material> materialArrayList = new ArrayList();
         
-        // Mapping de clases Entity a POJO
+        // Mapping de clases Entity a POJO.
         for (entities.Material matEntitie : materialEntitiesList){
             materialArrayList.add(util.MappingFromEntitieToPojo.materialFromEntityToPojo(matEntitie));
         }
         
-        // Actualizar lista de materiales
+        // Actualizar lista de materiales.
         this.materialList = materialArrayList;
         
-        /*Llenando la lista de objetivos*/
+        //  Llenando la lista de objetivos.
         List<entities.Objetivo> listaObjetivosEntities= objetivoFacade.findAll();
         ArrayList<Objetivo> objetivoList = new ArrayList();
         
+        // Mapping de clases Entity a POJO.
         for (entities.Objetivo objetivoEntity : listaObjetivosEntities )
             objetivoList.add(util.MappingFromEntitieToPojo.objetivoFromEntityToPojo(objetivoEntity));
         
+        // Acualizar lista de objetivos.
         listaObjetivos = objetivoList;
                 
     }
@@ -150,33 +154,67 @@ public class ConsultarObjetivoManagedBean implements Serializable {
         return null;
     }
     
+    /**
+     * Listener del evento Fila seleccionada de la tabla de objetivo.
+     * En caso de seleccionar un objetivo, obtener todos los materiales
+     * de dicho objetivo, y mapearlos a clases POJO para su posterior
+     * procesamiento y posible edición.
+     * 
+     * @param event SelectEvent Evento de fila seleccionada
+     */
     public void handleRowSelect(SelectEvent event){
+        // Obtener el objeto seleccionado
         Objetivo obj = (Objetivo) event.getObject();
+        
+        //  Buscar todos los materiales asociados al objetivo seleccionado.
+        //  Utilizar EJB objetivoMaterialFacade
         List<entities.ObjetivoMaterial> omList = objetivoMaterialFacade.buscarPorObjetivo(obj.getCodObjetivo());
+        
+        // Nuevo Array de Clases POJO SelectedMaterial 
         ArrayList<SelectedMaterial> smArray = new ArrayList();
+        
+        // Para cada material relacionado con el objetivo seleccionado.
         for(entities.ObjetivoMaterial om : omList){
+            
+            // Hacer mapping con clase POJO y añadirlo al array.
             smArray.add(util.MappingFromEntitieToPojo.selectedMaterialFromMaterialObjetivoToPojo(om));
         }
+        
+        //  Setear al objetivo la nueva lista de clases SelectedMaterial, las cuales son los materiales
+        //  del objetivo seleccionado.
         obj.setMaterialList(smArray);
+        
+        // Setear objetivo como nuevo objetivo seleccionado
         selectedObjetivo = obj;
     }
     
+    // Mapping de la prioridad del objetivo seleccionado a String.
     public String verPrioridad(){
+        // Si el objetivo seleccionado es null, retornar nada.
         if (selectedObjetivo == null)
             return null;
+        
+        // Mapping de la prioridad del objetivo seleccionado.
         return verPrioridadTabla(selectedObjetivo.getPrioridad());
     }
     
+    //  Eliminar un material de la lista de materiales del objetivo.
     public void removeMaterialObjetivo(SelectedMaterial item){
         selectedObjetivo.removeMaterial(item);
     }
     
+    // Añadir un nuevo material al objetivo seleccionado.
     public void addMaterialObjetivo(){
+        // Nuevo material
         SelectedMaterial sm = new SelectedMaterial();
+        
+        // Setear el codigo del nuevo material en base al material escogido.
         sm.setCodMaterial(tempSelectedMaterial.getCodMaterial());
+        
+        // Si material no está en la lista, sobre-escribir la cantidad.
         if ( ! selectedObjetivo.isMaterialInObjetivo(sm) ){
-            sm.setCantidad(cantSelectedMaterial);
-            selectedObjetivo.addMaterial(sm);
+            sm.setCantidad(cantSelectedMaterial);           // Sobre-escribir cantidad.
+            selectedObjetivo.addMaterial(sm);               // Añadir material.
         }
     }
 }
